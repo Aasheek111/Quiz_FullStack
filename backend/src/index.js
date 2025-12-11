d4b15466e38b61dbd4571dd2f5f251ae7c2f0825
Aashik Gautam <technicalaashik111@gmail.com>
2025-12-11 21:15:22 +0545

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,20 @@
+import cors from 'cors'
+import 'dotenv/config'
 const express = require('express');
 const { connectDB } = require('./config/db');
 const app = express()
 const port = 3000;
+app.use(cors());
+app.use (express.json());
+
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
 
 app.listen(port, () => {
   console.log(`App running in http://localhost:${port}`)
