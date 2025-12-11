5418f93960bdbb8f27bbe8a4a996e00b5ab2edb7
Aashik Gautam <technicalaashik111@gmail.com>
2025-12-11 21:12:09 +0545

    mongo connected

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const uri =
-    "mongodb+srv://technicalaashik111_db_user:Nayapassword1704@@cluster0.lvaziwy.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://technicalaashik111_db_user:naya@cluster0.5b5ertb.mongodb.net/?appName=Cluster0";
 
   await mongoose
     .connect(uri)
