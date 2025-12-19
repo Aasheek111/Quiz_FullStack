ffe446eadacec774c5ebb71ce95275c846addb65
Aashik Gautam <technicalaashik111@gmail.com>
2025-12-19 20:22:14 +0545

    commit

--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -1,9 +1,22 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema=new mongoose.Schema({
-    user:String,
-    password:String,
-    email:String
-
-})
-module.exports=mongoose.model('User',UserSchema);
+const UserSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique:true
+  },
+});
+module.exports = mongoose.model("User", UserSchema);
+export default mongoose.model.User;
