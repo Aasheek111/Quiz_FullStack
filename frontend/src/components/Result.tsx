ae17219306f18058cd101ec2e1de8d9e815c6c40
Aashik Gautam <technicalaashik111@gmail.com>
2026-01-06 22:40:40 +0545

    results ui start

--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Result() {
@@ -16,24 +16,39 @@ function Result() {
     data: Result[];
   }
   const [data, setData] = useState<Result[]>([]);
+  // const getData = async () => {
+  //   const res = await axios.get<Api_res>(
+  //     "http://localhost:3001/result/displayall"
+  //   );
+  //   setData(res.data.data);
+  // };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get<Api_res>(
+          "http://localhost:3001/result/displayall"
+        );
+        setData(res.data.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-  const getData = async () => {
-    const res = await axios.get<Api_res>(
-      "http://localhost:3001/result/displayall"
-    );
-    setData(res.data.data);
-  };
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <h1>Results:</h1>
+    <div className="h-screen w-full bg-slate-600 flex justify-center items-center text-white text-2xl">
+      <div className=" max-w-2xl  bg-slate-900 p-6 rounded-2xl">
+        <h1>Results:</h1>
 
-      {data.map((val, ind) => (
-        <div key={ind}>
-          <h1>{val.technology}</h1>
-          <h1>{val.score}</h1>
-        </div>
-      ))}
+        {data.map((val, ind) => (
+          <div key={ind} className="flex">
+            <h1>{val.technology}{" = "}</h1>
+            <h1>{val.score}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
