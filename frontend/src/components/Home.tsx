b5f799f559f126c16c48e12e78adecc5513d1717
Aashik Gautam <technicalaashik111@gmail.com>
2026-01-20 20:45:28 +0545

    stagger

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,65 +1,69 @@
-import axios from "axios";
-import React, { useState } from "react";
+import { useState } from "react";
 import Questions from "./Questions";
-import { div } from "motion/react-client";
-import {motion} from 'framer-motion'
+
+import { motion } from "framer-motion";
 function Home() {
   const [level, setLevel] = useState<string>("");
   const [tech, setTech] = useState<string>("");
-  const [selected,setSelected]=useState(false);
+  const [selected, setSelected] = useState(false);
 
-  const parentVarient={
-    show:{
-      opacity:1,
-      transition:{
-        staggerChildren:0.1,
-        delayChildren:0.3
-      }
+  const parentVarient = {
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
     },
 
-    hide:{
-      opacity:0
-    }
-  }
-  const childVarient={
-    show:{
-      opacity:1,
-      x:9
-
+    hide: {
+      opacity: 0,
     },
-    hide:{
-      opacity:0,
-      x:-10
-    }
-  }
+  };
+  const childVarient = {
+    show: {
+      opacity: 1,
+      x: 5,
+    },
+    hide: {
+      opacity: 0,
+      x: 0,
+    },
+  };
 
-  const submitVal=()=>{
+  const submitVal = () => {
     setSelected(true);
-  }
+  };
 
-  const tec = ["HTML", "JS", "CSS", "REACT", "NEXT","NODE"];
+  const tec = ["HTML", "JS", "CSS", "REACT", "NEXT", "NODE"];
   const levels = ["Beginner", "Intermediate", "Pro"];
 
-  return (
-
-     selected?<div>
-      <Questions data={{level,tech}}/>
-      </div>
-      :
-
+  return selected ? (
+    <div>
+      <Questions data={{ level, tech }} />
+    </div>
+  ) : (
     <div className="min-h-[92vh] w-full bg-slate-700 flex justify-center items-center text-white">
       <div className="min-w-2xl  bg-slate-900  p-10 rounded-2xl text-2xl">
         <h1 className="text-center bold text-3xl p-4">QUIZ</h1>
         <div className="">
           <h1 className="w-full p-3">Technology</h1>
-          <motion.div className="grid grid-cols-2 gap-3 " variants={parentVarient}>
+          <motion.div
+            className="grid grid-cols-2 gap-3 "
+            variants={parentVarient}
+            initial="hide"
+            animate="show"
+          >
             {tec.map((val, i) => (
-
               <motion.button
-              variants={childVarient}
-              initial="hide"
-              animate="show"
-                className={` cursor-pointer rounded-lg p-3 ${tech==val?" bg-green-600":"bg-slate-500  hover:bg-slate-400 "} hover:scale-105 transition-all `}
+                variants={childVarient}
+                animate={{
+                  transition: {
+                    ease: "easeInOut",
+                  },
+                }}
+                // initial="hide"
+                // animate="show"
+                className={` cursor-pointer rounded-lg p-3 ${tech == val ? " bg-green-600" : "bg-slate-500  hover:bg-slate-400 "} hover:scale-105 transition-all `}
                 key={i}
                 onClick={() => setTech(val)}
               >
@@ -74,7 +78,7 @@ function Home() {
           <div className="grid grid-cols-3 gap-3">
             {levels.map((val, i) => (
               <button
-                className={`rounded-lg transition-all p-3 cursor-pointer hover:scale-105 ${level==val?"bg-red-600":"bg-slate-500  hover:bg-slate-400 "}`}
+                className={`rounded-lg transition-all p-3 cursor-pointer hover:scale-105 ${level == val ? "bg-red-600" : "bg-slate-500  hover:bg-slate-400 "}`}
                 key={i}
                 onClick={() => setLevel(val)}
               >
@@ -88,21 +92,20 @@ function Home() {
           <button
             disabled={!level || !tech}
             className="p-3  mt-7 cursor-pointer min-w-50 rounded-2xl bg-orange-500 transition-all hover:scale-105 hover:bg-orange-500 m-4 disabled:bg-orange-300"
-            onClick={()=>submitVal()}
+            onClick={() => submitVal()}
           >
             Start
           </button>
           <div className="text-lg">
-            -- You selected {" "}
-            <span className="font-extrabold text-green-600">{tech}</span>{" "}Technology and <span className="font-extrabold text-red-600">{level}</span>{" "} level
+            -- You selected{" "}
+            <span className="font-extrabold text-green-600">{tech}</span>{" "}
+            Technology and{" "}
+            <span className="font-extrabold text-red-600">{level}</span> level
           </div>
         </div>
       </div>
     </div>
-  
   );
-
-
 }
 
 export default Home;
